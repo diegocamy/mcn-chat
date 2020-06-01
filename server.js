ee3225f7d3b56caaed0d9905f84709c0f9ed4c57
diegocamy <diegocamyy@gmail.com>
2020-05-31 21:49:58 -0300

    -

--- server.js
+++ server.js
@@ -130,6 +130,10 @@ const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
